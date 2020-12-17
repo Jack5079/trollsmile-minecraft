6c4a66fa0e7cfef8fe2fadb51bc919e2e1857878
Jack <29169102+Jack5079@users.noreply.github.com>
2020-12-17 10:37:27 -0500

    add chests

--- commands/pathfinding/mine.ts
+++ commands/pathfinding/mine.ts
@@ -2,14 +2,21 @@ import { Bot, Message } from '../../utils/types'
 import { Vec3 } from 'vec3'
 
 export async function run (this: Bot, message: Message, args: string[]): Promise<string | void> {
-  await new Promise(resolve=>this.collectBlock.cancelTask(resolve))
+  await new Promise(resolve => this.collectBlock.cancelTask(resolve))
   if (!this.pathfinder.isMoving()) {
     const times = Number(args.join('').replace(/\D/g, '')) || 1
     await new Promise(resolve => this.collectBlock.collect(this.findBlocks({
       matching: block => block.name === args.filter(string => !Number(string)).join('_').replace(/\d/g, '').toLowerCase() && block.diggable,
       maxDistance: 1000,
       count: times
-    }).map(vec => this.blockAt(vec as unknown as Vec3)).filter((block): block is any => block !== null), resolve))
+    }).map(vec => this.blockAt(vec as unknown as Vec3)).filter((block): block is any => block !== null), {
+      chestLocations: this.findBlocks({
+        matching: block => block.name === 'chest',
+        maxDistance: 1000,
+        count: 1000
+      }) as unknown as Vec3[]
+    }, resolve))
+
     return 'done you stupid bitch'
   } else throw "Already performing a task!"
 }
---
+++ commands/pathfinding/pickup.ts
@@ -0,0 +1,18 @@
+import { Bot } from '../../utils/types'
+import { Vec3 } from 'vec3'
+
+export async function run (this: Bot): Promise<string | void> {
+  await new Promise(resolve => this.collectBlock.cancelTask(resolve))
+  if (!this.pathfinder.isMoving()) {
+    await new Promise(resolve => this.collectBlock.collect(Object.values(this.entities).filter(ent => ent.objectType === 'Item'), {
+      chestLocations: this.findBlocks({
+        matching: block => block.name === 'chest',
+        maxDistance: 1000,
+        count: 1000
+      }) as unknown as Vec3[]
+    }, resolve))
+    return 'done you stupid bitch'
+  } else throw "Already performing a task!"
+}
+export const help = 'steals items on the ground'
+export const aliases = ['drops']
