b62cdf73ea9cbcf13e9b0523c5f348c326532ecb
Jack <29169102+Jack5079@users.noreply.github.com>
2020-12-17 09:40:49 -0500

    a

--- commands/pathfinding/mine.ts
+++ commands/pathfinding/mine.ts
@@ -3,36 +3,34 @@ import { goals, Movements } from 'mineflayer-pathfinder'
 import MinecraftData from 'minecraft-data'
 import { Vec3 } from 'vec3'
 
-export function run (this: Bot, message: Message, args: string[]): Promise<string | void> {
-  return new Promise(async (resolve, reject) => {
-    if (!this.pathfinder.isMoving()) {
-      const times = Number(args.join('').replace(/\D/g, '')) || 1
-      const mcData = MinecraftData(this.version)
-      for (const index of Array(times).keys()) {
-        // We create different movement generators for different type of activity
-        const defaultMove = new Movements(this, mcData)
-        const [position] = this.findBlocks({
-          matching: block => block.name === args.filter(string => !Number(string)).join('_').replace(/\d/g, '').toLowerCase() && block.diggable,
-          maxDistance: 1000
-        }) as unknown as Vec3[]
-        console.log(position)
-        if (position) {
-          this.pathfinder.setMovements(defaultMove)
-          await new Promise(resolve => this.pathfinder.goto(new goals.GoalNear(position.x, position.y, position.z, 2), resolve))
-          const block = this.blockAt(position) as any
-          await this.dig(block)
-          if (message.author) {
-            this.chat(`Mined a block! ${(index + 1 / times) * 100}% complete.`)
-          }
-        } else {
-          if (message.author) {
-            this.chat('could not find a ' + args.join(' '))
-          }
+export async function run (this: Bot, message: Message, args: string[]): Promise<string | void> {
+  if (!this.pathfinder.isMoving()) {
+    const times = Number(args.join('').replace(/\D/g, '')) || 1
+    const mcData = MinecraftData(this.version)
+    for (const index of Array(times).keys()) {
+      // We create different movement generators for different type of activity
+      const defaultMove = new Movements(this, mcData)
+      const [position] = this.findBlocks({
+        matching: block => block.name === args.filter(string => !Number(string)).join('_').replace(/\d/g, '').toLowerCase() && block.diggable,
+        maxDistance: 1000
+      }) as unknown as Vec3[]
+      console.log(position)
+      if (position) {
+        this.pathfinder.setMovements(defaultMove)
+        await new Promise(resolve => this.pathfinder.goto(new goals.GoalNear(position.x, position.y, position.z, 2), resolve))
+        const block = this.blockAt(position) as any
+        await this.dig(block)
+        if (message.author) {
+          this.chat(`Mined a block! ${(index + 1 / times) * 100}% complete.`)
+        }
+      } else {
+        if (message.author) {
+          this.chat('could not find a ' + args.join(' '))
         }
       }
-      resolve('done you stupid bitch')
-    } else return reject("Already performing a task!")
-  })
+    }
+    return 'done you stupid bitch'
+  } else throw "Already performing a task!"
 }
 export const help = 'steals an item'
 export const aliases = ['get']
