a1996501043983b75e137f84ae696fdb0099157c
Jack <29169102+Jack5079@users.noreply.github.com>
2020-12-18 22:34:48 -0500

    empty string name never worked

--- commands/help.ts
+++ commands/help.ts
@@ -3,4 +3,4 @@ export async function run (this: Bot, message: Message, args: string[]): Promise
   return this.commands.map((cmd, name)=>`${name} ${cmd.aliases?.length ? `(AKA ${cmd.aliases.join()})`: ''} - ${cmd.help}`).join('\n')
 }
 export const help = 'see all da commands'
-export const aliases = ['cmds', '', 'commands', 'all']
+export const aliases = ['cmds', 'commands', 'all']
