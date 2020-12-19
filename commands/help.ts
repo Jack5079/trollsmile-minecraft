5a76a4bcaba9322eea16fea4dfc4d7be2f5c328e
Jack <29169102+Jack5079@users.noreply.github.com>
2020-12-18 22:35:04 -0500

    a

--- commands/help.ts
+++ commands/help.ts
@@ -1,5 +1,5 @@
-import { Bot, Message } from '../utils/types'
-export async function run (this: Bot, message: Message, args: string[]): Promise<string | void> {
+import { Bot } from '../utils/types'
+export async function run (this: Bot): Promise<string> {
   return this.commands.map((cmd, name)=>`${name} ${cmd.aliases?.length ? `(AKA ${cmd.aliases.join()})`: ''} - ${cmd.help}`).join('\n')
 }
 export const help = 'see all da commands'
